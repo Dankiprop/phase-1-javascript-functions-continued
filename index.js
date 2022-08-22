3b218eff78a9def91ed970cf147bbf483430bb12
dankiprop <dankiproh@gmail.com>
2022-08-22 14:11:23 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity) {
+  if (activity !== undefined) {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  else {
+    return "This Saturday, I want to roller-skate!";
+  }
+
+}
+
+function mondayWork(activity) {
+if (activity !== undefined) {
+  return `This Monday, I will ${activity}.`
+} else {
+  return "This Monday, I will go to the office."
+}
+}
+
+const wrapAdjective = (result = '*') => {
+
+  return (phrase = "a hard worker") => {
+  return `You are ${result}${phrase}${result}!`;
+  
+}
+
+
+}
